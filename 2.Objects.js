eed9da042f9be2b287ab6da13bd135f991111947
GenuinelyMeizner <frederikmpetersen@gmail.com>
2022-09-04 23:29:52 +0200

    intro excersises done

---
+++ 2.Objects.js
@@ -0,0 +1,53 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const alienMessage = {
+	message: "Hello, earthling! I bring peace."
+};
+
+// Log the message 
+
+console.log(alienMessage.message)
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+
+const person = {
+	name: "Frederik",
+	age: 27
+}
+
+console.log(person)
+
+// --------------------------------------
+// Exercise 3 - Add a key-value pair 
+
+const stackOverflow = {};
+
+stackOverflow.key1 = "secret";
+
+console.log(stackOverflow)
+
+// make a rule called isAllowed and let the value be true
+
+// --------------------------------------
+// Exercise 4 - Remove a key-value pair 
+
+const thisSong = {
+	description: "The best song in the world."
+}
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+delete thisSong.description;
+
+console.log(thisSong);
+
+
+// --------------------------------------
+
+
